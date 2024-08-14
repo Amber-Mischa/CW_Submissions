c1e7e4920df83ebe15a2469f8ddb6015253e8941
Amber-Mischa <akanthack29@gmail.com>
2024-08-14 10:30:38 -1000

    Update Hello_World.java
    Magic :D

--- Hello_World.java
+++ Hello_World.java
@@ -1,1 +1,13 @@
-
+/* 
+ * This code is for the first assignment. 
+ * It prints out two things then I submit it.
+ */
+public class Hello_World {
+	public static void main(String[] args) {
+		//Task 1: Print out "Hello World"
+		System.out.print("Hello World");
+		//Task 2: Print out "Hello World" and
+		//then print it again on the next line
+		System.out.println("Hello World");
+		System.out.print("Hello World"); }
+}
