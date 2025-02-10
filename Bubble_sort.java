26a9fdd7a1daa45bbd9ec654975cc030b5afe384
Amber-Mischa <akanthack29@gmail.com>
2025-02-10 13:05:07 -1000

    Create Bubble_sort.java

---
+++ Bubble_sort.java
@@ -0,0 +1,45 @@
+/**
+ * This class is to make the bubble sorting array algorithm.
+ * @author amberkanthack
+ */
+public class Sorting_Algorithm {
+
+	public static void main(String[] args) {
+		int[] arr = {9,2,5,1};
+		
+		bubbleSort(arr);
+		for(int i = 0; i <arr.length;i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+	//BUBBLE SORT AN ARRAY
+	public static void bubbleSort(int[] nums)
+	{
+    //variables for the method
+		int[] copyNums = nums;
+		int count = -1;
+    //makes sure everything is sorted
+		while(count != 0)
+		{
+      //resets counter
+			count = 0;
+      //checks for everything in the array
+			for(int i =0; i<copyNums.length-1; i++)
+			{
+        //sees if it is in order
+				if(copyNums[i] > copyNums[i+1])
+				{
+          //swaps the numbers
+					int temp = copyNums[i];
+					copyNums[i] = copyNums[i+1];
+					copyNums[i+1] = temp;
+					count++;
+				}
+			}
+		}
+	}
+}
+	
