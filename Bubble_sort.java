ae9269ac03c71b101927a54f812babd7fdbec182
Amber-Mischa <akanthack29@gmail.com>
2025-02-10 20:03:31 -1000

    Update Bubble_sort.java

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -18,21 +18,21 @@ public class Sorting_Algorithm {
 	//BUBBLE SORT AN ARRAY
 	public static void bubbleSort(int[] nums)
 	{
-    //variables for the method
+   		//variables for the method
 		int[] copyNums = nums;
 		int count = -1;
-    //makes sure everything is sorted
+  		//makes sure everything is sorted
 		while(count != 0)
 		{
-      //resets counter
+      			//resets counter
 			count = 0;
-      //checks for everything in the array
+     			//checks for everything in the array
 			for(int i =0; i<copyNums.length-1; i++)
 			{
-        //sees if it is in order
+        			//sees if it is in order
 				if(copyNums[i] > copyNums[i+1])
 				{
-          //swaps the numbers
+          				//swaps the numbers
 					int temp = copyNums[i];
 					copyNums[i] = copyNums[i+1];
 					copyNums[i+1] = temp;
